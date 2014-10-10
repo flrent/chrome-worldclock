1a7865062da4d585d0d85b03c69fe5391124af64
flrent <flrent@me.com>
2014-10-09 23:02:25 -0700

    Ready for 1.0.0

--- app/scripts/app/controllers/controllers.js
+++ app/scripts/app/controllers/controllers.js
@@ -1,6 +1,27 @@
 angular.module('popup')
-  .controller('MainController', ['$scope', function($scope) {
+  .controller('MainController', ['$scope',  '$rootScope', 'Storage', function($scope, $rootScope, Storage) {
+      $scope.showAddForm = true;
+
       $scope.zones = moment.tz.names();
+
+      Storage.get(function(timezone) {
+      		if(timezone==null) {
+      			$scope.displayTime = moment(new Date().getTime()).format('MMMM Do YYYY');
+      			$scope.displayHour = moment(new Date().getTime()).format('hh:mm:ss a');
+      		}
+      		else {
+      			$scope.current = timezone;
+      			$scope.displayTime = moment(new Date().getTime()).tz(timezone).format('MMMM Do YYYY');
+      			$scope.displayHour = moment(new Date().getTime()).tz(timezone).format('hh:mm a');
+      		}
+      });
+
+      $scope.changeTimeZone = function() {
+      		$scope.displayTime = moment(new Date().getTime()).tz(this.current).format('MMMM Do YYYY');
+      		$scope.displayHour = moment(new Date().getTime()).tz(this.current).format('hh:mm a');
+      		Storage.set(this.current);
+      }
+
 
       $scope.contribute = function() {
         chrome.tabs.create({
--- app/scripts/app/services/storage.js
+++ app/scripts/app/services/storage.js
@@ -1,6 +1,15 @@
 angular.module('popup', [])
   .service('Storage', ['$rootScope', function($rootScope) {
 
+    function _set(timezone) {
+    	localStorage['current'] = timezone;
+    }
+
+    function _get(cb) {
+    	cb(localStorage['current'] || null);
+    }
     return {
+    	set: _set,
+    	get: _get
     };
   }]);
